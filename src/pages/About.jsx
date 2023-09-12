ad24f2fbdc3f7a5cf9639d85ee20564ab7cfa916
Rajeev Nayan <118424833+nrajeevofficial@users.noreply.github.com>
2023-09-12 12:46:38 +0530

    Add files via upload

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,23 @@
+// Import the useTranslation hook from the "react-i18next" library.
 import { useTranslation } from "react-i18next";
 
+// Define a functional component called "About".
 function About() {
+  // Destructure the "t" function from the useTranslation hook.
   const { t } = useTranslation();
+
   return (
-    <div className="container ">
+    <div className="container">
+      {/* Create a card element */}
       <div class="card mt-5">
         <div className="card-title text-center">
+          {/* Display a translated heading using the "t" function */}
           <h2>{t("brief")}</h2>
+          {/* Display an image with an alt text, the source of the image is translated */}
           <img id="image" src={t("image")} alt="" />
         </div>
         <div className="card-body">
+          {/* Display a translated content text using the "t" function */}
           <h3 className="content">{t("content")}</h3>
         </div>
       </div>
@@ -17,4 +25,5 @@ function About() {
   );
 }
 
+// Export the About component as the default export of this module.
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
+// Import the useTranslation hook from the "react-i18next" library.
 import { useTranslation } from "react-i18next";
 
+// Define a functional component called "Home".
 function Home() {
+  // Destructure the "t" function from the useTranslation hook to access translations.
   const { t } = useTranslation();
+
   return (
+    // Create a centered text element with a top margin and a "main" class.
     <div className="text-center mt-5 main">
+      {/* Display a translated heading using the "t" function */}
       <h1>{t("home")}</h1>
     </div>
   );
 }
 
+// Export the Home component as the default export of this module.
 export default Home;
